7dce8c2c2dd394f149907911e9b9d437ec0ad81c
m_farahmand <m.farahmand@outlook.com>
2022-12-22 15:54:42 +0330

    feat(): add webservice

---
+++ SMDRReader/WebService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMDRReader
+{
+    public class WebService
+    {
+        public string Url { get; }
+        public string Key { get; }
+
+        public WebService(string url,string key)
+        {
+            Url = url;
+            Key = key;
+        }
+
+        public async Task<string> SendRequest(string data)
+        {
+            WebRequest request = WebRequest.Create(Url);
+            request.Method = "POST";
+            request.ContentType = "text/x-gwt-rpc; charset=utf-8";
+            request.Headers.Add("Authorization", $"Bearer{Key}");
+            byte[] byteArray = Encoding.UTF8.GetBytes(data);
+            request.ContentLength = byteArray.Length;
+            Stream dataStream = await request.GetRequestStreamAsync();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+            return "";
+        }
+    }
+}
