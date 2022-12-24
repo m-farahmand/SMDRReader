fa1606c00f3f2ba357e1ef45933864e7fb84f5f3
m_farahmand <m.farahmand@outlook.com>
2022-12-24 15:38:19 +0330

    feat(): add util service

---
+++ SMDRReader/UtilService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMDRReader
+{
+    static class Util
+    {
+        public static Properties.Settings GetSettings()
+        {
+            return Properties.Settings.Default;
+        }
+
+        public static string GetExtKey(string Ext)
+        {
+            foreach (var item in GetSettings().ExtKey)
+            {
+                if (item.StartsWith(Ext))
+                    return item.Split('=')[1];//return second part of ExtKey 
+            }
+            Console.WriteLine("Use default key");
+            return GetSettings().DefaultKey;
+        }
+    }
+}
